9cfa91d78cb421d34c368fb55cbbb997fbbdc316
Ms-Afifa <ms.afifasultana.1@gmail.com>
2026-03-05 02:27:11 +0500

    Create project.js

---
+++ project.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const tabButtons = document.querySelectorAll(".tab-btn");
+  const tabContents = document.querySelectorAll(".tab-content");
+
+  tabButtons.forEach(btn => {
+    btn.addEventListener("click", () => {
+      tabButtons.forEach(b => b.classList.remove("active"));
+      tabContents.forEach(c => c.classList.remove("active"));
+
+      btn.classList.add("active");
+      document
+        .getElementById(btn.dataset.tab)
+        .classList.add("active");
+    });
+  });
+});
